e55876194a3317331bcda3acba4d636c798230ed
caseyboehlke <71906610+caseyboehlke@users.noreply.github.com>
2020-10-19 00:17:56 -0500

    Update VoterDatabaseTestSuite.java

--- VoterDatabaseTestSuite.java
+++ VoterDatabaseTestSuite.java
@@ -1,10 +1,10 @@
 // --== CS400 File Header Information ==--
-// Name: Casey Boehlke
-// Email: cboehlke@wisc.edu
+// Name: Connor William Dyjach
+// Email:dyjach@wisc.edu
 // Team: CB
 // TA: Yeping
-// Lecturer: Florian Heimerl
-// Notes to Grader: None
+// Lecturer: Gary Dahl
+// Notes to Grader:
 
 import static org.junit.jupiter.api.Assertions.*;
 import java.text.SimpleDateFormat;
@@ -14,99 +14,125 @@ import org.junit.jupiter.api.Test;
 class VoterDatabaseTestSuite {
 
   /**
-   * Meant for use by Front End Developers
-   * 
-   * These tests are for the Voter class, specifically testing the two different constructors and
-   * the getter methods.
+   * FOR DATA WRANGLERS: Tests the Voter class constructors and methods
    */
   @Test
   void voterTest() {
-
-    // This is me, Casey Boehlke. My data will be used to test the methods.
-    Voter voter = new Voter(createDate(20010117), 'D', "Casey Boehlke");
-
-    if (!voter.getName().equals("Casey Boehlke")) {
-      fail("The name returned and the name used to create the object did not match.");
+    
+    Voter completeVoter = new Voter(createDate(20010725), 'D', "Connor"); //Voter(Date, char, String)
+    
+    // 1. Get values that the Voter was created with 
+    
+    if (!completeVoter.getName().equals("Connor")) {
+      fail("The name that was returned did not match the name that Voter was " + "created with");
     }
-
-    if (!(voter.getParty() == 'D')) {
-      fail("The party returned and the party used to create the object did not match.");
+    
+    if (!(completeVoter.getParty() == 'D')) {
+      fail("The party that was returned did not match the party that the Voter was " + "created with");
     }
-
-    if (!(voter.getBirthday().equals(createDate(20010117)))) {
-      fail("The birth date returned and the birth date used to create the pbject did not match.");
+    
+    if (!(completeVoter.getBirthday().equals(createDate(20010725)))) {
+      fail("The birthday that was returned did not match the birthday that the Voter was " + "created with");
     }
-
-    // This voter object was only given a birth date, and should have the default data specified for
-    // a voter with just a birth date.
-    Voter birthdayVoter = new Voter(createDate(19680615));
-
-    if (!(birthdayVoter.getName() == null)) {
-      fail("The name returned and the name used to create the object did not match.");
+    
+    Voter incompleteVoter = new Voter (createDate(20010825)); //Voter(Date)
+    
+    if (!(incompleteVoter.getName() == null)) {
+      fail("The name that was returned did not match the name that Voter was " + "created with");
     }
-
-    if (!(birthdayVoter.getParty() == 'p')) {
-      fail("The party returned and the party used to create the object did not match.");
+    
+    if (!(incompleteVoter.getParty() == 'p')) {
+      fail("The party that was returned did not match the party that the Voter was " + "created with");
     }
-
-    if (!(birthdayVoter.getBirthday().equals(createDate(20010825)))) {
-      fail("The birth date returned and the birth date used to create the pbject did not match.");
+    
+    if (!(incompleteVoter.getBirthday().equals(createDate(20010825)))) {
+      fail("The birthday that was returned did not match the birthday that the Voter was " + "created with");
     }
 
   }
-
+  
   /*
-   * Meant for use by Back End Developers
-   * 
-   * This set of tests specifically tests the getVoter method and the add method.
+   * FOR BACK END DEVELOPERS: Tests getVoter method, add method
    */
   @Test
   void getVoterTest() {
+    // try to get an existing voter
 
-    BackEnd testBackEnd = new BackEnd();
+    VoterDatabase voterDatabase = new VoterDatabase();
 
-    // This voter is me again. My data will once again be used to test the methods.
-    Voter voter = new Voter(createDate(20010117), 'D', "Casey Boehlke");
-
-
-    testBackEnd.addVoter(20010117, "Casey Boehlke", 'D');
+    Voter a = new Voter(createDate(20010725), 'D', "Connor");
+    Voter b = new Voter(createDate(20010726), 'D', "Joe");
+    Voter c = new Voter(createDate(20010724), 'D', "Bob");
 
 
-    if (!(testBackEnd.getVoter(20010117).equals(voter))) {
-      fail("'voter' should have been returned, but was not.");
+   
+    voterDatabase.insertVoter(20010726, "Joe", 'D');
+    voterDatabase.insertVoter(20010725, "Connor", 'D');
+    voterDatabase.insertVoter(20010724,"Bob", 'D');
+
+
+
+    if (!(voterDatabase.getVoter(20010725).getBirthday().equals(a.getBirthday()))
+        || !(voterDatabase.getVoter(20010725).getParty() == 'D')
+        || !(voterDatabase.getVoter(20010725).getName().equals(a.getName()))) {
+      fail(voterDatabase.getVoter(20010725).toString() + " VS " + a.toString());
     }
 
-    if (testBackEnd.getVoter(19680615) == null) {
-      fail("This voter does not exist, and thus cannot be retrieved from the database.");
+    try {
+
+      voterDatabase.getVoter(19990826);
+
+    } catch (Exception e) {
+
+      return;
     }
+    fail("cannot get a voter that DNE");
 
   }
 
   /*
-   * Meant for use by Back End Developers
-   * 
-   * These simple tests specifically test the size aspect of the voter database.
+   * FOR BACK END DEVELOPERS: Tests size
    */
   @Test
   void getSizeTest() {
 
-    BackEnd testBackEnd = new BackEnd();
+    VoterDatabase voterDatabase = new VoterDatabase();
 
-    testBackEnd.addVoter(20010117, "Casey Boehlke", 'D');
-
-    if (testBackEnd.size != 1) {
-      fail("The size at this moment should be one, but it is not.");
+    if (voterDatabase.getVoterDatabaseSize() != 0) {
+      fail("Size is incorrect");
     }
 
-    testBackEnd.addVoter(19690420, "John Doe", 'R');
-    testBackEnd.addVoter(19700521, "Jane Doe", 'R');
+    voterDatabase.insertVoter(20010725, "Connor", 'D');
 
-    if (testBackEnd.size != 3) {
-      fail("The size at this moment should be three, but it is not.");
+    if (voterDatabase.getVoterDatabaseSize() != 1) {
+      fail("Size is incorrect");
     }
 
 
   }
+  
+  /*
+   * FOR BACK END DEVELOPERS: Tests size
+   */
+  @Test
+  void collisionTest() {
+    
+    VoterDatabase voterDatabase = new VoterDatabase();
+    
+    Voter a = new Voter(createDate(20010725), 'D', "Connor");
+    Voter b = new Voter(createDate(20010725), 'D', "Connor");
+    
+    voterDatabase.insertVoter(20010725, "Connor", 'D');
+    
+    if (voterDatabase.insertVoter(20010725, "Connor", 'D')) {
+      fail("The voter should NOT be added to the database");
+    }
+    
+    
+  }
+  
+
+
 
   /*
    * This method creates a date format.
@@ -115,14 +141,14 @@ class VoterDatabaseTestSuite {
    */
   public Date createDate(int birthDate) {
     try {
-      int year = birthDate / 10000;
-      int month = (birthDate % 10000) / 100;
-      int date = birthDate % 100;
-      String formattedDate = year + "-" + month + "-" + date; // Creates a formatted date.
-
-      SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-      return sdf.parse(formattedDate);
-    } catch (java.text.ParseException ex) {
+    int year = birthDate/10000;
+    int month = (birthDate%10000)/100;
+    int date = birthDate%100;
+    String formattedDate = year+"-"+month+"-"+date; //Creates a fromatted date.
+    
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+    return sdf.parse(formattedDate);
+    }catch(java.text.ParseException ex) {
       return null;
     }
   }
