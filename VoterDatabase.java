d6ba616d45dde43501f3e53a0ce3809666c35141
Lindsay Dyjach <71901869+ldyjach@users.noreply.github.com>
2020-10-20 13:23:56 -0500

    Add files via upload

--- VoterDatabase.java
+++ VoterDatabase.java
@@ -22,23 +22,33 @@ public class VoterDatabase {
    * @param none
    */
   public VoterDatabase() {
-    // loads a number of default books to the library
+    //creates a voter database
     voterDatabase = new RedBlackTree<Voter>();
     this.size = 0;
   }
 
-
+  /**
+   * Inserts a voter into the database given a birthdate, name, and political party data
+   * 
+   * @param birthdate is an int number that is the voter's birthday
+   * @param name is a String of the voter's name
+   * @param party is a char value representing the voter's political party associations
+   * 
+   * @return true if the voter is inserted succesfully into the database, else false
+   */
   public boolean insertVoter(int birthdate, String name, char party) {
+    //Date object
     Date formatDate;
     if (createDate(birthdate) != null) {
-      formatDate = createDate(birthdate);
+      formatDate = createDate(birthdate); //formats the int birthdate into a date
     } else {
       return false;
     }
 
     Voter voter = new Voter(formatDate, party, name);
+    
     try {
-      voterDatabase.insert(voter);
+      voterDatabase.insert(voter); //inserts the voter into the database
       this.size++;
       return true;
     } catch (IllegalArgumentException ex) {
@@ -48,40 +58,76 @@ public class VoterDatabase {
     }
   }
 
+  /**
+   * Gets a voter from the database when the birthday is searched for
+   * 
+   * @param birthdate is an int number that is the voter's birthday
+
+   * 
+   * @return foundVoter if voter is found
+   */
   public Voter getVoter(int birthdate) {
 
     // Creating a valid date first.
     Date formatDate;
+    
     if (createDate(birthdate) != null) {
-      formatDate = createDate(birthdate);
+      formatDate = createDate(birthdate); // formats the int birthdate into type date
     } else {
       formatDate = null;
     }
     // When it's incorrectly parsed.
     if (formatDate == null)
       return null;
-
-    return traverseRBT(voterDatabase.root, formatDate);
+    
+    try {
+      traverseRBT(voterDatabase.root, formatDate);
+    }
+    catch(NoSuchElementException e) {
+      
+    }
+    return traverseRBT(voterDatabase.root, formatDate); //return the voter when looking through the entire tree for it
   }
 
+  /**
+   * Traverses the voter database (red black tree) in order to find the matching node as 
+   * the birthdate of the user entered when trying to get/ lookup a voter
+   * 
+   * @param root is the root of the RedBlackTree
+   * @param birthdate is a Date object that is the voter to gets birthdate
+   * 
+   * @return a voter object that is the one that matches the birthdate of the given voter
+   */
   private Voter traverseRBT(RedBlackTree.Node<Voter> root, Date birthdate) {
     
     RedBlackTree.Node<Voter> currentNode = root;
+    //no voter in the database so none can be gotten
     if (currentNode == null) {
       throw new NoSuchElementException("There is no voter in the database");
     }
+    
+    // if the current node matches that of that of the birthdate, return that node
     if (currentNode.data.compareTo(new Voter(birthdate)) == 0) {
       return currentNode.data;
+      
+      //if the current node does not match traverse down the subtrees
+      
     } else if (currentNode.data.compareTo(new Voter(birthdate)) > 0) {
       // left subtree
-      return traverseRBT(currentNode.leftChild, birthdate);
+      return traverseRBT(currentNode.leftChild, birthdate); //recursively traverse through left subtree
     } else {
       // right subtree
-      return traverseRBT(currentNode.rightChild, birthdate);
+      return traverseRBT(currentNode.rightChild, birthdate); //recursively traverse through right subtree
     }
   }
 
-  // This method creates a date format.
+  /**
+   * Formats the int birthdate into a date object to be stored in the voterdatabase
+   * 
+   * @param birthdate is the int to be stored as a date
+   * 
+   * @return a formatted date object representing the voter's birthday
+   */
   public Date createDate(int birthDate) {
     try {
       int year = birthDate / 10000;
