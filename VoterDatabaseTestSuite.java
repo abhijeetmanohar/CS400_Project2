99aa48a420fabaeb1ae7866fecfef35ae15cb5dd
dyjach <71941573+dyjach@users.noreply.github.com>
2020-10-20 12:32:53 -0500

    Add files via upload

--- VoterDatabaseTestSuite.java
+++ VoterDatabaseTestSuite.java
@@ -18,39 +18,44 @@ class VoterDatabaseTestSuite {
    */
   @Test
   void voterTest() {
-    
-    Voter completeVoter = new Voter(createDate(20010725), 'D', "Connor"); //Voter(Date, char, String)
-    
-    // 1. Get values that the Voter was created with 
-    
+
+    Voter completeVoter = new Voter(createDate(20010725), 'D', "Connor"); // Voter(Date, char,
+                                                                          // String)
+
+    // 1. Get values that the Voter was created with
+
     if (!completeVoter.getName().equals("Connor")) {
       fail("The name that was returned did not match the name that Voter was " + "created with");
     }
-    
+
     if (!(completeVoter.getParty() == 'D')) {
-      fail("The party that was returned did not match the party that the Voter was " + "created with");
+      fail("The party that was returned did not match the party that the Voter was "
+          + "created with");
     }
-    
+
     if (!(completeVoter.getBirthday().equals(createDate(20010725)))) {
-      fail("The birthday that was returned did not match the birthday that the Voter was " + "created with");
+      fail("The birthday that was returned did not match the birthday that the Voter was "
+          + "created with");
     }
-    
-    Voter incompleteVoter = new Voter (createDate(20010825)); //Voter(Date)
-    
+
+    Voter incompleteVoter = new Voter(createDate(20010825)); // Voter(Date)
+
     if (!(incompleteVoter.getName() == null)) {
       fail("The name that was returned did not match the name that Voter was " + "created with");
     }
-    
+
     if (!(incompleteVoter.getParty() == 'p')) {
-      fail("The party that was returned did not match the party that the Voter was " + "created with");
+      fail("The party that was returned did not match the party that the Voter was "
+          + "created with");
     }
-    
+
     if (!(incompleteVoter.getBirthday().equals(createDate(20010825)))) {
-      fail("The birthday that was returned did not match the birthday that the Voter was " + "created with");
+      fail("The birthday that was returned did not match the birthday that the Voter was "
+          + "created with");
     }
 
   }
-  
+
   /*
    * FOR BACK END DEVELOPERS: Tests getVoter method, add method
    */
@@ -65,10 +70,10 @@ class VoterDatabaseTestSuite {
     Voter c = new Voter(createDate(20010724), 'D', "Bob");
 
 
-   
+
     voterDatabase.insertVoter(20010726, "Joe", 'D');
     voterDatabase.insertVoter(20010725, "Connor", 'D');
-    voterDatabase.insertVoter(20010724,"Bob", 'D');
+    voterDatabase.insertVoter(20010724, "Bob", 'D');
 
 
 
@@ -78,15 +83,15 @@ class VoterDatabaseTestSuite {
       fail(voterDatabase.getVoter(20010725).toString() + " VS " + a.toString());
     }
 
-    try {
+   
 
-      voterDatabase.getVoter(19990826);
+      if (voterDatabase.getVoter(19990826) != null) {
+        fail("cannot get a voter that DNE");
+      }
+      
 
-    } catch (Exception e) {
-
-      return;
-    }
-    fail("cannot get a voter that DNE");
+    
+    
 
   }
 
@@ -110,27 +115,26 @@ class VoterDatabaseTestSuite {
 
 
   }
-  
+
   /*
    * FOR BACK END DEVELOPERS: Tests size
    */
   @Test
   void collisionTest() {
-    
+
     VoterDatabase voterDatabase = new VoterDatabase();
-    
+
     Voter a = new Voter(createDate(20010725), 'D', "Connor");
     Voter b = new Voter(createDate(20010725), 'D', "Connor");
-    
+
     voterDatabase.insertVoter(20010725, "Connor", 'D');
-    
+
     if (voterDatabase.insertVoter(20010725, "Connor", 'D')) {
       fail("The voter should NOT be added to the database");
     }
-    
-    
+
+
   }
-  
 
 
 
@@ -141,14 +145,14 @@ class VoterDatabaseTestSuite {
    */
   public Date createDate(int birthDate) {
     try {
-    int year = birthDate/10000;
-    int month = (birthDate%10000)/100;
-    int date = birthDate%100;
-    String formattedDate = year+"-"+month+"-"+date; //Creates a fromatted date.
-    
-    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-    return sdf.parse(formattedDate);
-    }catch(java.text.ParseException ex) {
+      int year = birthDate / 10000;
+      int month = (birthDate % 10000) / 100;
+      int date = birthDate % 100;
+      String formattedDate = year + "-" + month + "-" + date; // Creates a formated date.
+
+      SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+      return sdf.parse(formattedDate);
+    } catch (java.text.ParseException ex) {
       return null;
     }
   }
